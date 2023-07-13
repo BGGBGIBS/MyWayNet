911b22c4d363217476988aaecccba594e4e02074
BGGBGIBS <83280831+BGGBGIBS@users.noreply.github.com>
2023-07-13 17:18:30 +0200

    controllers part II

---
+++ Controllers/EventController.cs
@@ -0,0 +1,173 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyWayNet.Models;
+
+namespace MyWayNet.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly MyWayContext _context;
+
+        public EventController(MyWayContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Event
+        public async Task<IActionResult> Index()
+        {
+            var myWayContext = _context.Events.Include(e => e.Institution).Include(e => e.Occupation);
+            return View(await myWayContext.ToListAsync());
+        }
+
+        // GET: Event/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Events == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events
+                .Include(e => e.Institution)
+                .Include(e => e.Occupation)
+                .FirstOrDefaultAsync(m => m.EventId == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // GET: Event/Create
+        public IActionResult Create()
+        {
+            ViewData["InstitutionId"] = new SelectList(_context.Institutions, "InstitutionId", "InstitutionId");
+            ViewData["OccupationId"] = new SelectList(_context.Occupations, "OccupationId", "OccupationId");
+            return View();
+        }
+
+        // POST: Event/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventId,EventBegin,EventEnd,OccupationId,InstitutionId,EventType")] Event event)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@event);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["InstitutionId"] = new SelectList(_context.Institutions, "InstitutionId", "InstitutionId", event.InstitutionId);
+            ViewData["OccupationId"] = new SelectList(_context.Occupations, "OccupationId", "OccupationId", event.OccupationId);
+            return View(@event);
+        }
+
+        // GET: Event/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Events == null)
+            {
+                return NotFound();
+            }
+
+            var event = await _context.Events.FindAsync(id);
+            if (event == null)
+            {
+                return NotFound();
+            }
+            ViewData["InstitutionId"] = new SelectList(_context.Institutions, "InstitutionId", "InstitutionId", event.InstitutionId);
+            ViewData["OccupationId"] = new SelectList(_context.Occupations, "OccupationId", "OccupationId", event.OccupationId);
+            return View(@event);
+        }
+
+        // POST: Event/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("EventId,EventBegin,EventEnd,OccupationId,InstitutionId,EventType")] Event event)
+        {
+            if (id != event.EventId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(event);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventExists(event.EventId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["InstitutionId"] = new SelectList(_context.Institutions, "InstitutionId", "InstitutionId", event.InstitutionId);
+            ViewData["OccupationId"] = new SelectList(_context.Occupations, "OccupationId", "OccupationId", event.OccupationId);
+            return View(event);
+        }
+
+        // GET: Event/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Events == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events
+                .Include(e => e.Institution)
+                .Include(e => e.Occupation)
+                .FirstOrDefaultAsync(m => m.EventId == id);
+            if (event == null)
+            {
+                return NotFound();
+            }
+
+            return View(event);
+        }
+
+        // POST: Event/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Events == null)
+            {
+                return Problem("Entity set 'MyWayContext.Events'  is null.");
+            }
+            var event = await _context.Events.FindAsync(id);
+            if (event != null)
+            {
+                _context.Events.Remove(event);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventExists(long id)
+        {
+          return (_context.Events?.Any(e => e.EventId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/RecordController.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyWayNet.Models;
+
+namespace MyWayNet.Controllers
+{
+    public class RecordController : Controller
+    {
+        private readonly MyWayContext _context;
+
+        public RecordController(MyWayContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Record
+        public async Task<IActionResult> Index()
+        {
+            var myWayContext = _context.Records.Include(r => r.Event).Include(r => r.Grade).Include(r => r.Skill);
+            return View(await myWayContext.ToListAsync());
+        }
+
+        // GET: Record/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Records == null)
+            {
+                return NotFound();
+            }
+
+            var record = await _context.Records
+                .Include(r => r.Event)
+                .Include(r => r.Grade)
+                .Include(r => r.Skill)
+                .FirstOrDefaultAsync(m => m.RecordId == id);
+            if (rrecord == null)
+            {
+                return NotFound();
+            }
+
+            return View(rrecord);
+        }
+
+        // GET: Record/Create
+        public IActionResult Create()
+        {
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId");
+            ViewData["GradeId"] = new SelectList(_context.Grades, "GradeId", "GradeId");
+            ViewData["SkillId"] = new SelectList(_context.Skills, "SkillId", "SkillId");
+            return View();
+        }
+
+        // POST: Record/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RecordId,UserId,EventId,SkillId,GradeId")] Record record)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(record);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", record.EventId);
+            ViewData["GradeId"] = new SelectList(_context.Grades, "GradeId", "GradeId", record.GradeId);
+            ViewData["SkillId"] = new SelectList(_context.Skills, "SkillId", "SkillId", record.SkillId);
+            return View(rrecord);
+        }
+
+        // GET: Record/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Records == null)
+            {
+                return NotFound();
+            }
+
+            var record = await _context.Records.FindAsync(id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", record.EventId);
+            ViewData["GradeId"] = new SelectList(_context.Grades, "GradeId", "GradeId", record.GradeId);
+            ViewData["SkillId"] = new SelectList(_context.Skills, "SkillId", "SkillId", record.SkillId);
+            return View(record);
+        }
+
+        // POST: Record/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("RecordId,UserId,EventId,SkillId,GradeId")] Record record)
+        {
+            if (id != record.RecordId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(record);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecordExists(record.RecordId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "EventId", "EventId", record.EventId);
+            ViewData["GradeId"] = new SelectList(_context.Grades, "GradeId", "GradeId", record.GradeId);
+            ViewData["SkillId"] = new SelectList(_context.Skills, "SkillId", "SkillId", record.SkillId);
+            return View(record);
+        }
+
+        // GET: Record/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Records == null)
+            {
+                return NotFound();
+            }
+
+            var record = await _context.Records
+                .Include(r => r.Event)
+                .Include(r => r.Grade)
+                .Include(r => r.Skill)
+                .FirstOrDefaultAsync(m => m.RecordId == id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+
+            return View(record);
+        }
+
+        // POST: Record/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Records == null)
+            {
+                return Problem("Entity set 'MyWayContext.Records'  is null.");
+            }
+            var record = await _context.Records.FindAsync(id);
+            if (record != null)
+            {
+                _context.Records.Remove(record);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecordExists(long id)
+        {
+          return (_context.Records?.Any(e => e.RecordId == id)).GetValueOrDefault();
+        }
+    }
+}
